a36d814aebe2ebb806c77b76760ed718f0862a16
keikoyoshida <34933156+keikoyoshida@users.noreply.github.com>
2018-03-29 23:34:05 -0700

    Revert "Revert "Implement user sign in, off and sign up function""

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,35 @@
+//modal
+$(document).on('turbolinks:load', function () {
+  //ログインをクリックしたら
+  $(".signIn").on("click", function(){
+    $(".modal__signUp, .modal__bg").hide() ;
+    $(".modal__bg").remove() ;
+    //body内の最後に<div id="modal-bg"></div>を挿入
+    $("body").append('<div class="modal__bg"></div>');
+    //モーダルウィンドウを表示
+    $(".modal, .modal__signIn, .modal__bg").fadeIn("slow");
+    //背景をクリックしたらモーダルを閉じる
+    $(".modal__bg").on("click", function(){
+      $(".modal, .modal__signIn, .modal__bg").fadeOut("slow",function(){
+        //挿入した<div id="modal-bg"></div>を削除
+        $(".modal__bg").remove() ;
+      });
+    });
+  });
+  //新規登録
+  $(".signUp").on("click", function(){
+    $(".modal__signIn, .modal__bg").hide() ;
+    $(".modal__bg").remove() ;
+    //body内の最後に<div id="modal-bg"></div>を挿入
+    $("body").append('<div class="modal__bg"></div>');
+    //モーダルウィンドウを表示
+    $(".modal, .modal__signUp, .modal__bg").fadeIn("slow");
+    //画面のどこかをクリックしたらモーダルを閉じる
+    $(".modal__bg").on("click", function(){
+      $(".modal, .modal__signUp, .modal__bg").fadeOut("slow",function(){
+        //挿入した<div id="modal-bg"></div>を削除
+        $(".modal__bg").remove() ;
+      });
+    });
+  });
+});
