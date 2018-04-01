9c63ef6e6e36f6d41f02b93634d7265a500c6560
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-01 02:09:31 -0700

    modify after_sign_in_path description

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,12 +7,12 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def create
-    binding.pry
     self.resource = warden.authenticate!(auth_options)
     set_flash_message!(:notice, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    # respond_with resource, location: after_sign_in_path_for(resource)
+    redirect_to root_path
   end
 
 # 認証が失敗した場合に呼び出されるアクション
@@ -24,7 +24,6 @@ class Users::SessionsController < Devise::SessionsController
     # 失敗時に recall に設定したパスのアクションが呼び出されるので変更
     # { scope: resource_name, recall: "#{controller_path}#new" } # デフォルト
     { scope: resource_name, recall: "#{controller_path}#login_error" }
-    binding.pry
   end
 
 end
