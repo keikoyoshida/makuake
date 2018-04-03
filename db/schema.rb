03c89d08a681f857421b68f475f4b8d026a9f711
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-03 12:21:36 -0700

    modify - rake db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402212505) do
+ActiveRecord::Schema.define(version: 20180402213217) do
 
   create_table "project_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "type",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type"], name: "index_project_types_on_type", unique: true, using: :btree
+  end
+
+  create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title",                         null: false
+    t.string   "heading_movie",                 null: false
+    t.text     "content_html",    limit: 65535, null: false
+    t.string   "content_image"
+    t.time     "deadline",                      null: false
+    t.integer  "user_id"
+    t.integer  "project_type_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["deadline"], name: "index_projects_on_deadline", using: :btree
+    t.index ["project_type_id"], name: "index_projects_on_project_type_id", using: :btree
+    t.index ["title"], name: "index_projects_on_title", using: :btree
+    t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -38,4 +54,6 @@ ActiveRecord::Schema.define(version: 20180402212505) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "projects", "project_types"
+  add_foreign_key "projects", "users"
 end
