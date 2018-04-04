e415c5b7cc69d19dea45c0c2597f307ac6dffe91
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-03 23:20:25 -0700

    describe projects#create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,12 +4,28 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to root_path, notice: 'プロジェクトが作成されました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :heading_movie, :content_html, :content_image, :deadline, :project_type_id).merge(user_id: current_user.id)
   end
 
 end
