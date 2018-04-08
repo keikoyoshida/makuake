54044bc1a59d4dce050a04fdc9b37a0e0477997a
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-08 00:19:50 -0700

    set carrier wave-magic in image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::Magic
+  process :set_magic_content_type => [true]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
