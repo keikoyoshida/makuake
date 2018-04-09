cc8b8bad796b979e4331a438234c16a143e7eac3
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-08 22:12:49 -0700

    modify association in project_tag, tag and project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-  # has_many :tags, through: :project_tag
-  # has_many :project_tags
+  has_many :tags, through: :project_tags
+  has_many :project_tags, dependent: :destroy
   # has_many :supporters, reports, support_comments, couses, products
   belongs_to :user
   belongs_to :project_type
